60ea601f49e69645a22b784f80506fb8555379a1
Rodrigoclc <146961365+Rodrigoclc@users.noreply.github.com>
2025-10-07 14:31:19 -0300

    refactor: Remoção das instancias para usar o arquivo de DI

--- backend/src/routes/auth.ts
+++ backend/src/routes/auth.ts
@@ -1,15 +1,10 @@
 import { Router } from 'express';
-import { AuthController } from '../controllers/AuthController';
-import { AuthService } from '../services/AuthService';
+import { authController } from '../di/container';
 import { authMiddleware } from '../middleware/auth';
-import { UserRepository } from '../repository/UserRepository';
 import { userValidation } from '../utils/validation'
 import { validateBody } from '../middleware/validation';
 
 const router = Router();
-const userRepository = new UserRepository();
-const authService = new AuthService(userRepository);
-const authController = new AuthController(authService);
 
 router.post('/register', validateBody(userValidation.create), authController.register);
 router.post('/login', validateBody(userValidation.login), authController.login);
