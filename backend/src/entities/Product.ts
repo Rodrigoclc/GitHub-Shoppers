efe340b13b29d35cb69b653923f14c1d408ddac7
Rodrigoclc <146961365+Rodrigoclc@users.noreply.github.com>
2025-10-07 10:59:54 -0300

    refactor: Alteração na entidade de produto

--- backend/src/entities/Product.ts
+++ backend/src/entities/Product.ts
@@ -1,39 +1,8 @@
-import {
-  Entity,
-  PrimaryGeneratedColumn,
-  Column,
-  CreateDateColumn,
-  UpdateDateColumn,
-  ManyToOne,
-  OneToMany,
-} from "typeorm";
-import { User } from "./User";
-import { Purchase } from "./Purchase";
-
-@Entity("products")
-export class Product {
-  @PrimaryGeneratedColumn()
-  id!: number;
-
-  @Column()
-  name!: string;
-
-  @Column("decimal", { precision: 10, scale: 2 })
-  price!: number;
-
-  @Column({ default: 0 })
-  current_quantity!: number;
-
-  @CreateDateColumn()
-  created_at!: Date;
-
-  @UpdateDateColumn()
-  updated_at!: Date;
-
-  // Relações
-  @ManyToOne(() => User, (user) => user.products)
-  user!: User;
-
-  @OneToMany(() => Purchase, (purchase) => purchase.product)
-  purchases!: Purchase[];
+export interface Product {
+  id: number;
+  nome: string;
+  preco: number;
+  qtd_atual: number;
+  created_at: Date;
+  updated_at: Date;
 }
