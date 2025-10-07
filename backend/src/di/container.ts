a8b1fa75b2dbf2889df3ddb69e4e31bbb30dd687
Rodrigoclc <146961365+Rodrigoclc@users.noreply.github.com>
2025-10-07 15:58:27 -0300

    refactor: Atualização no arquivo de configuração de DI

--- backend/src/di/container.ts
+++ backend/src/di/container.ts
@@ -1,13 +1,23 @@
 import { AuthService } from '../services/AuthService';
 import { AuthController } from '../controllers/AuthController';
 import { UserRepository } from '../repository/UserRepository';
+import { ProductService } from '../services/ProductService';
+import { ProductRepository } from '../repository/ProductRepository';
+import { ProductController } from '../controllers/ProductController';
 
 const userRepository = new UserRepository();
 const authService = new AuthService(userRepository);
 const authController = new AuthController(authService);
 
+const productRepository = new ProductRepository();
+const productService = new ProductService(productRepository);
+const productController = new ProductController(productService);
+
 export {
   userRepository,
   authService,
   authController,
+  productController,
+  productRepository,
+  productService,
 };
