07d1e664645a7a1f1bb2a5930ea913239515aec2
Rodrigoclc <146961365+Rodrigoclc@users.noreply.github.com>
2025-10-07 18:21:03 -0300

    refactor: Adicionado role na entidade e nas DTOs necessarias

--- backend/src/dto/AuthPayloadDto.ts
+++ backend/src/dto/AuthPayloadDto.ts
@@ -1,4 +1,7 @@
+import { UserRole } from "../entities/User";
+
 export interface AuthPayloadDto {
   userId: number;
   email: string;
+  role: UserRole;
 }
--- backend/src/entities/User.ts
+++ backend/src/entities/User.ts
@@ -2,6 +2,9 @@ export interface User {
   id: number;
   email: string;
   password_hash: string;
+  role: UserRole;
   created_at: Date;
   updated_at: Date;
 }
+
+export type UserRole = 'admin' | 'user';
