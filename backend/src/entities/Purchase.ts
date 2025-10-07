80cffaf28bf9242150eb7bfcbe072248a760cb78
Rodrigoclc <146961365+Rodrigoclc@users.noreply.github.com>
2025-10-07 10:42:37 -0300

    refactor: Alteração na entidade de compra

--- backend/src/entities/Purchase.ts
+++ backend/src/entities/Purchase.ts
@@ -1,25 +1,7 @@
-import { Entity, PrimaryGeneratedColumn, Column, CreateDateColumn, UpdateDateColumn, ManyToOne } from 'typeorm';
-import { Product } from './Product';
-import { User } from './User';
-
-@Entity('purchases')
-export class Purchase {
-  @PrimaryGeneratedColumn()
-  id!: number;
-
-  @Column()
-  buyer_github_login!: string;
-
-  @CreateDateColumn()
-  created_at!: Date;
-
-  @UpdateDateColumn()
-  updated_at!: Date;
-
-  // Relações
-  @ManyToOne(() => Product, product => product.purchases)
-  product!: Product;
-
-  @ManyToOne(() => User, user => user.purchases)
-  user!: User;
+export interface Purchase {
+  id: number;
+  item_id: number;
+  comprador_github_login: string;
+  created_at: Date;
+  updated_at: Date;
 }
