5146e81cf94720442fd5e0decd6c4755d222749c
Rodrigoclc <146961365+Rodrigoclc@users.noreply.github.com>
2025-10-07 16:00:02 -0300

    refactor: Atualização na rota de itens para chamar a controller

--- backend/src/routes/item.ts
+++ backend/src/routes/item.ts
@@ -1,8 +1,11 @@
 import { Router } from 'express';
+import { validateBody } from '../middleware/validation';
+import { itemValidation } from '../utils/validation';
+import { productController} from '../di/container'
 
 const router = Router();
 
-router.post('/', (req, res)=> { return res.send('Create item endpoint'); });
-router.get('/', (req, res)=> { return res.send('Get all items endpoint'); });
+router.post('/', validateBody(itemValidation.create), productController.create);
+router.get('/', productController.getAll);
 
 export default router;
