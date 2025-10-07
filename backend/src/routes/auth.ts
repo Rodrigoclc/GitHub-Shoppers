c09961f512a8ce09f01bdc8f2a239a713e670830
Rodrigoclc <146961365+Rodrigoclc@users.noreply.github.com>
2025-10-07 14:07:28 -0300

    refactor:  Adicionado validações de paramentros

--- backend/src/routes/auth.ts
+++ backend/src/routes/auth.ts
@@ -3,15 +3,16 @@ import { AuthController } from '../controllers/AuthController';
 import { AuthService } from '../services/AuthService';
 import { authMiddleware } from '../middleware/auth';
 import { UserRepository } from '../repository/UserRepository';
+import { userValidation } from '../utils/validation'
+import { validateBody } from '../middleware/validation';
 
 const router = Router();
-// Instanciando as dependências
 const userRepository = new UserRepository();
 const authService = new AuthService(userRepository);
 const authController = new AuthController(authService);
 
-router.post('/register', authController.register);
-router.post('/login', authController.login);
+router.post('/register', validateBody(userValidation.create), authController.register);
+router.post('/login', validateBody(userValidation.login), authController.login);
 
 router.get('/profile', authMiddleware, authController.getProfile);
 router.put('/change-password', authMiddleware, authController.changePassword);
