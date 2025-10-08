31b9f9a4852924bdec137b359b521e9c49798a85
Rodrigoclc <146961365+Rodrigoclc@users.noreply.github.com>
2025-10-07 21:23:06 -0300

    refactor: Correcao no bug na validacao da rota

--- backend/src/routes/item.ts
+++ backend/src/routes/item.ts
@@ -3,10 +3,11 @@ import { validateBody } from '../middleware/validation';
 import { itemValidation } from '../utils/validation';
 import { productController} from '../di/container'
 import { authMiddleware } from '../middleware/auth';
+import { requireAdmin } from '../middleware/authorization';
 
 const router = Router();
 
-router.post('/', authMiddleware, validateBody(itemValidation.create), productController.create);
+router.post('/', authMiddleware, requireAdmin, validateBody(itemValidation.create), productController.create);
 router.get('/', authMiddleware, productController.getAll);
 
 export default router;
